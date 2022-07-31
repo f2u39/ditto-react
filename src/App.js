19517d9d0d11cbf2aef116e455f1817faf853bed
fuu <unifuusawatari@gmail.com>
2022-07-31 19:29:26 +0900

    dry

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import Cards from "./components/Cards";
 
 // import the Container Component from the semantic-ui-react
 // import { Container } from "semantic-ui-react";
@@ -7,20 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cards />
     </div>
   );
 }
